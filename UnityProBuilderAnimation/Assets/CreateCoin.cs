54ddd283f8c6ebf49caa999a2773e4ca16aa367a
KID421 <suchild7@gmail.com>
2019-12-16 14:43:15 +0800

    完成版

---
+++ UnityProBuilderAnimation/Assets/CreateCoin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CreateCoin : MonoBehaviour
+{
+    [Header("金幣")]
+    public GameObject coin;
+    [Header("音效")]
+    public AudioClip soundCoin;
+
+    private AudioSource aud;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// 生成金幣
+    /// </summary>
+    public void CreateCoinMethod()
+    {
+        aud.PlayOneShot(soundCoin);
+        Instantiate(coin);
+    }
+}

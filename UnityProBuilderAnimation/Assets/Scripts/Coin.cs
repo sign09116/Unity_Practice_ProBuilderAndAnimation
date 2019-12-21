19ca2ce4cb69acff705010138eb6ad92b33edc14
sign09116 <57554727+sign09116@users.noreply.github.com>
2019-12-21 14:01:24 +0800

    完成

---
+++ UnityProBuilderAnimation/Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    #region 屬性
+    [Header("金幣音效")]
+    public AudioClip clipCoin;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    #endregion
+
+
+    #region 事件
+    // Start is called before the first frame update
+    void Start()
+    {
+        Aud.PlayOneShot(clipCoin);
+        Destroy(gameObject,10f);
+    }
+
+}
+    #endregion
+
+    #region 方法
+    #endregion
+
---
+++ UnityProBuilderAnimation/Assets/Scripts/GM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    #region 屬性
+    public GameObject gameObj;
+    public Transform OBjtransform;
+    #endregion
+    #region 事件
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObj = Resources.Load<GameObject>("Coin");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+    public void Certate() 
+    {
+        Instantiate(gameObj, OBjtransform.position, OBjtransform.rotation);
+    }
+}
